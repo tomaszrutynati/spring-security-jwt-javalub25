544bf49cd35692ad2260b324257616f11c454578
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2021-05-29 09:40:42 +0200

    init commit

---
+++ src/main/java/pl/sda/springsecurityjwtjavalub25/SpringSecurityJwtJavalub25Application.java
@@ -0,0 +1,13 @@
+package pl.sda.springsecurityjwtjavalub25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtJavalub25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtJavalub25Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/springsecurityjwtjavalub25/SpringSecurityJwtJavalub25ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.springsecurityjwtjavalub25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtJavalub25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
